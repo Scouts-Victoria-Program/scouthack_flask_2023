4d87000a3d412837b0b323db4e64b44ef3f47991
Matthew Cengia <mattcen@mattcen.com>
2023-04-23 00:44:40 +1000

    Create basic flask app and move index.html to templates/
    At this point, we'll need to stop the `python -m http.server 80` server,
    and instead start using Flask. To use Flask:
    
    1.  Create a Python virtual environment:
    
        ```bash
        mattcen@scouthack:~/code$ python -m venv .venv
        mattcen@scouthack:~/code$
        ```
    
    2.  Activate the virtual environment:
    
        ```bash
        mattcen@scouthack:~/code$ source .venv/bin/activate
        (.venv) mattcen@scouthack:~/code$
        ```
    
    3.  Install Flask:
    
        ```bash
        (.venv) mattcen@scouthack:~/code$ pip install flask
        Collecting flask
        [...]
        (.venv) mattcen@scouthack:~/code$
        ```
    
    4.  Ensure our `website.py` is executable:
    
        ```bash
        (.venv) mattcen@scouthack:~/code$ chmod a+x website.py
        ```
    
        You can check that this worked correctly by running the following
        command:
    
        ```bash
        (.venv) mattcen@scouthack:~/code$ ls -l website.py
        -rwxrwxr-x 1 mattcen mattcen 289 Apr 23 02:03 website.py
        ```
    
        Note the 3 `x`es near the start; if you don't have those, you've
        done something wrong.
    
    5.  Finally, we can now run our Flask app:
    
        ```bash
        (.venv) mattcen@mattcen:~/code$ ./website.py
         * Serving Flask app 'website'
         * Debug mode: on
        WARNING: This is a development server. Do not use it in a production
        deployment. Use a production WSGI server instead.
         * Running on all addresses (0.0.0.0)
         * Running on http://127.0.0.1:80
         * Running on http://172.18.0.6:80
        Press CTRL+C to quit
         * Restarting with stat
         * Debugger is active!
         * Debugger PIN: 100-690-544
        ```
    
    6.  Now, if you browse to and/or refresh your website, it should be
        working again, except that it won't have any images, or styles.

---
+++ website.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index.html")
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=80, host="0.0.0.0")
